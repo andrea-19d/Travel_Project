81bca0f8b5d00ca2a7b9879b08cceeea0ffd6bb7
andrea-19d <138387838+andrea-19d@users.noreply.github.com>
2024-02-28 20:13:14 +0200

    Dashboard

---
+++ App/Controllers/AccountSettingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class AccountSettingsController : Controller
+    {
+        // GET: AccountSettings
+        public ActionResult AccountSettings()
+        {
+            return View();
+        }
+    }
+}
---
+++ App/Controllers/LogOutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class LogOutController : Controller
+    {
+        // GET: LogOut
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
