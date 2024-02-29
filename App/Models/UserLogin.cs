bb01d0889829cdc96bd481bcfbdb6909e77bcd95
AdrianaZolotcov <147655208+AdrianaZolotcov@users.noreply.github.com>
2024-02-29 18:18:20 +0200

    add

---
+++ App/Models/UserLogin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+  
+        public class userLogin
+        {
+        public string Credential;
+        public string Password;
+        }
+            
+
+    }
+
