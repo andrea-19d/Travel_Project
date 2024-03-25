7ddade724083f5cc9040751aa4e6efcf49f92e0d
AdrianaZolotcov <147655208+AdrianaZolotcov@users.noreply.github.com>
2024-03-25 22:22:56 +0200

    registrafion full

--- App/Controllers/RegisterPageController.cs
+++ App/Controllers/RegisterPageController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using App.Models;
+using BusinessLogic.Interfaces;
+using BusinessLogic;
+using Domain.Entities.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,64 @@ namespace App.Controllers
 {
     public class RegisterPageController : Controller
     {
-        // GET: RegisterPage
+        private readonly ISession _session;
+
+        public RegisterPageController()
+        {
+            var bl = new BussinesLogic();
+            _session = bl.GetSessionBL();
+        }
+
+        // GET: LogInPage (assuming you want to display the login form initially)
         public ActionResult Register()
         {
             return View();
+        }
+
+        public void Usera()
+        {
+            var credential = "dsadsa";
+            var password = "password";
+            var data = new userLogin
+            {
+                Credential = credential,
+                Password = password,
+            };
+            Register(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(userLogin login)
+        {
+            Usera();
+            if (ModelState.IsValid)
+            {
+                // Assuming userLogin is a model representing user credentials (email and password)
+                ULoginData data = new ULoginData
+                {
+                    Credential = login.Credential,
+                    Password = login.Password,
+                    LoginIp = Request.UserHostAddress,
+                    LoginDateTime = DateTime.Now,
+                };
+
+                var userLogin = _session.UserLogin(data);
+
+                if (userLogin.Status)
+                {
+                    // Successfully logged in, redirect to home or dashboard
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Login failed, add error message to ModelState
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
+
+            // If ModelState is not valid or login is unsuccessful, return to login view with error messages
+            return View(login);
         }
     }
 }
